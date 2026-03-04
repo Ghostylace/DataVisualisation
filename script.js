f5ac3cffe098ea48d7529fc18161b1738e81dbea
Cyril Escher <cyril.escher@lehre.roche.com>
2026-03-04 10:28:01 +0100

    Add jQuery and Bootstrap and filter data that don't have any value

--- script.js
+++ script.js
@@ -4,4 +4,46 @@ window.addEventListener("load", async () => {
     let response = await fetch(APIURL);
     let data = await response.json();
     console.log(data);
+    FilterData(data[1]);
 })
+function FilterData (data){
+    console.log(data);
+    var dataFilter = [];
+    for(let i = 0; i < data.length; i++){
+        if(data[i].value){
+            dataFilter.push(data[i]);
+        }
+    }
+    console.log(dataFilter);
+    SortDataByCountry(dataFilter);
+}
+function SortDataByCountry(data){
+    var dataCountry = [];
+    for(var i = 0; i < data.length; i++){
+        if(dataCountry.length){
+            var j = dataCountry.length -1;  
+            if(dataCountry[j].id != data[i].country.id){
+                dataCountry.push(data[i].country);
+            }
+        }
+        else{
+            dataCountry.push(data[i].country);
+        }
+    }
+    dataCountry.splice(0, 48);
+    console.log(dataCountry)
+    DisplayCountries(dataCountry);
+}
+function DisplayCountries(countries){
+    var dataHTML = $("#dataTableBody");
+    for(var i = 0; i < countries.length; i++){
+        var tEl = document.createElement("tr");
+        var tIdEl = document.createElement("td");
+        var tValEl = document.createElement("td");
+        tIdEl.innerHTML = countries[i].id;
+        tValEl.innerHTML = countries[i].value;
+        tEl.appendChild(tIdEl);
+        tEl.appendChild(tValEl);
+        dataHTML[0].appendChild(tEl);
+    }
+}
